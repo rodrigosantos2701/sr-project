9979fc1f0340a0b1c27965e1f2e9dd29b88c1b5f
rodrigosantos2701 <rodrigo2701@icloud.com>
2021-04-22 10:50:42 -0300

    [TEST] teste sigin

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -75,7 +75,7 @@ export default function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign in*
         </Typography>
 
         <form className={classes.form} ref={formData} onSubmit={handleSubmit}>
